689b3c4c08e0b5dac8e3d9b95007f8e0fd7a151e
Ali Samadi <alisamadi.096@gmail.com>
2025-04-27 11:27:12 -0500

    db folder done

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,5 +1,17 @@
+import db from "#db/client";
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
+  const sql = `
+  INSERT INTO employees (name, birthday, salary)
+  VALUES($1, $2, $3)
+  RETURNING *;
+`;
+
+  const {
+    rows: [employee],
+  } = await db.query(sql, [name, birthday, salary]);
+  return employee;
   // TODO
 }
 
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
-import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import db from "./client.js";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +8,16 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.book.title(),
+      birthday: faker.date.between({
+        from: "1980-01-01T00:00:00.000Z",
+        to: "2001-01-01T00:00:00.000Z",
+      }),
+      salary: faker.number.int({ min: 30000, max: 60000 }),
+    };
+
+    await createEmployee(employee);
+  }
 }
